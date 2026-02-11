10841bd714d1c97415211b48066292c1272a8d69
Yash <khandelwalyashayush@gmail.com>
2026-02-12 00:53:37 +0530

    adjusted paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './App.css'
 import MainPage from './Pages/Main-Page';
 import SearchPage from './Pages/Search-Parking-Page';
-import Signup from "./auth/Sign-up-auth";
-import Signin from "./auth/Sign-in-auth";
+import Signup from "./Pages/auth/Sign-up-auth";
+import Signin from "./Pages/auth/Sign-in-auth";
 
 function App() {
 
